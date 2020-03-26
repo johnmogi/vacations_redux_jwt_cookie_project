52b472becbd8755410977d6439c6084562392764
john <john@localhost.localdomain>
2020-03-26 17:42:01 +0200

    ready for redux

--- server/dal.js
+++ server/dal.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "q1w2e3",
   database: "tourist"
 });
 connection.connect(err => {
